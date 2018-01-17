c9467b08c6d52f566361c41a428f4cb8ca697b88
Marissa B <m1bertulfo@gmail.com>
2018-01-17 04:26:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase)
     return "I can\'t hear you!"
-  elseif (string === string.toUpperCase)
+  if (string === string.toUpperCase)
     return "YES INDEED!"
+  if (string === 'I love you, Grandma.')
+    return "I love you, too."
 }
